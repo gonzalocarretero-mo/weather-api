371846082f85d0ffaa7a64d6baf759e553e12935
Gonzalo Carretero <gonzac0512@gmail.com>
2025-09-29 16:07:50 +0200

    Fix Docker configuration to start redis container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Build stage
-FROM golang:1.23 AS builder
+FROM golang:1.25.1 AS builder
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download
@@ -10,5 +10,9 @@ RUN go build -o weather-api ./cmd/server
 # Final stage
 FROM debian:bookworm-slim
 WORKDIR /app
+# Install CA certificates so HTTPS works
+RUN apt-get update && apt-get install -y --no-install-recommends ca-certificates && rm -rf /var/lib/apt/lists/*
+
 COPY --from=builder /app/weather-api .
+COPY .env .
 CMD ["./weather-api"]
--- config/config.go
+++ config/config.go
@@ -1,11 +1,12 @@
 package config
 
 import (
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	ServerPort    string `mapstructure:"server_port"`
+	ServerPort    string `mapstructure:"SERVER_PORT"`
 	WeatherAPIKey string `mapstructure:"WEATHER_API_KEY"`
 	RedisURL      string `mapstructure:"REDIS_URL"`
 	LogType       string `mapstructure:"LOG_TYPE"`
@@ -14,9 +15,15 @@ type Config struct {
 func LoadConfig() (Config, error) {
 	var cfg Config
 	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
 	if err := viper.ReadInConfig(); err != nil {
-		return cfg, err
+		log.Warn().Msg("Error reading config file in config.go")
+		// return cfg, err
 	}
 	err := viper.Unmarshal(&cfg)
+	log.Info().Msg("Config loaded successfully")
+	log.Info().Msgf("Config RedisURL: %+v", cfg.RedisURL)
 	return cfg, err
 }
